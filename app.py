7e408749281d1c8587e2d1d5a35da1befbc262f7
stoky555 <gabriel.stoklas@gmail.com>
2022-04-30 22:00:13 +0100

    something

--- app.py
+++ app.py
@@ -29,9 +29,71 @@ ser = serial.Serial("/dev/ttyUSB0", 9600)
 ser.baudrate=9600
 
 read_ser = ser.readline()
-y=[]
 
-while 1==1:
-	print(float(read_ser))
-	read_ser=ser.readline()
-	ser.write(15)
+def background_thread(args):
+    count = 0    
+    btnV = 0
+    while True:
+        if args:
+            A = dict(args).get('A')
+        else:
+            A = 1
+
+        btnV = dict(args).get('btn_value')
+        print(A)
+        print(args)
+        print(btnV)
+        socketio.sleep(2)
+        count += 1
+        value = ser.readline()
+        if btnV:
+            print(dataDict)
+            socketio.emit('my_response',
+                          {'data': dataDict, 'count': count},
+                          namespace='/test')  
+
+@app.route('/')
+def index():
+    return render_template('index.html', async_mode=socketio.async_mode)
+  
+@socketio.on('my_event', namespace='/test')
+def test_message(message):   
+    session['receive_count'] = session.get('receive_count', 0) + 1 
+    session['A'] = message['value']    
+    emit('my_response',
+         {'data': message['value'], 'count': session['receive_count']})
+    ser.write(message['value'])
+ 
+@socketio.on('disconnect_request', namespace='/test')
+def disconnect_request():
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': 'Disconnected!', 'count': session['receive_count']})
+    disconnect()
+
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    global thread
+    with thread_lock:
+        if thread is None:
+            thread = socketio.start_background_task(target=background_thread, args=session._get_current_object())
+    emit('my_response', {'data': 'Connected', 'count': 0})
+
+@socketio.on('click_eventStart', namespace='/test')
+def db_message(message):   
+    session['btn_value'] = 1
+    #print(session['click_eventStart'])
+    print(session)
+
+@socketio.on('click_eventStop', namespace='/test')
+def db_message(message):   
+    session['btn_value'] = 0
+    #print(session['click_eventStop'])
+    print(session)
+
+@socketio.on('disconnect', namespace='/test')
+def test_disconnect():
+    print('Client disconnected', request.sid)
+
+if __name__ == '__main__':
+    socketio.run(app, host="0.0.0.0", port=80, debug=True)
